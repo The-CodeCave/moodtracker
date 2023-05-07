70e663bf69ad89540b6bf72aa0b350a43b45c9c8
aguenter <andrei.guenter@gmail.com>
2023-05-07 12:45:59 -1000

    implemented social button for passkey

--- lib/login/widgets/passkey_sign_in_button.dart
+++ lib/login/widgets/passkey_sign_in_button.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:moodtracker/constants.dart';
 
-import '../bloc/login_bloc.dart';
+import '../model/login_provider.dart';
+import 'social_login_button.dart';
 
 class PassKeySignInButton extends StatelessWidget {
   final double fontSize = loginButtonHeight * 0.43;
   final double progressIndicatorStrokeWidth = 2;
-  final Color fontColor = Colors.white;
-  final Color buttonBackground = Colors.black;
+  final Color foregroundColor = Colors.white;
+  final Color backgroundColor = Colors.black;
   final String signIn = "Sign in with Passkey";
 
   const PassKeySignInButton({
@@ -18,50 +18,15 @@ class PassKeySignInButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<LoginBloc, LoginState>(
-      builder: (context, state) {
-        return SizedBox(
-          height: loginButtonHeight,
-          child: FilledButton(
-            style: FilledButton.styleFrom(backgroundColor: buttonBackground),
-            onPressed: () async {
-              if (state is LoginLoadingPasskey) {
-                return;
-              }
-              context.read<LoginBloc>().add(LoginWithPasskey());
-            },
-            child: state is LoginLoadingPasskey
-                ? Center(
-                    child: SizedBox(
-                      height: fontSize,
-                      width: fontSize,
-                      child: CircularProgressIndicator(
-                        strokeWidth: progressIndicatorStrokeWidth,
-                        color: fontColor,
-                      ),
-                    ),
-                  )
-                : Padding(
-                    padding: const EdgeInsets.all(defaultSpacerSize),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          FontAwesomeIcons.key,
-                          color: fontColor,
-                          size: fontSize,
-                        ),
-                        SizedBox(width: defaultSpacerSize),
-                        Text(
-                          signIn,
-                          style: TextStyle(color: fontColor, fontSize: fontSize, fontWeight: FontWeight.w500),
-                        ),
-                      ],
-                    ),
-                  ),
-          ),
-        );
-      },
+    return SocialLoginButton(
+      provider: LoginProvider.applePasskey,
+      icon: Icon(
+        FontAwesomeIcons.key,
+        color: foregroundColor,
+        size: fontSize,
+      ),
+      backgroundColor: backgroundColor,
+      foregroundColor: foregroundColor,
     );
   }
 }
